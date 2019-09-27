47f1124bbdcc467cb23237134bf15ea692a3db1a
Jumpyrhombus <elimulqueen@gmail.com>
2019-09-26 21:37:01 -0500

    Created the home page and set up basic layout

--- Game Utilities/Game Utilities/HomePage.xaml.cs
+++ Game Utilities/Game Utilities/HomePage.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,14 +14,13 @@ using System.Windows.Shapes;
 namespace Game_Utilities
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for HomePage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class HomePage : Page
     {
-        public MainWindow()
+        public HomePage()
         {
             InitializeComponent();
-            NavFrame.NavigationService.Navigate(new HomePage());
         }
     }
 }
